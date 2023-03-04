c84b8f51e34503fc49506df6a3d09ba46715af68
IleD94 <80365922+IleD94@users.noreply.github.com>
2023-03-04 16:19:58 +0100

    Add files via upload

---
+++ cluedo/html/search/variables_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['res_94',['res',['../classcluedo__fsm_1_1hint__gen.html#abae781e5b5d4fcfec93082ae5f2ce8ac',1,'cluedo_fsm::hint_gen']]],
+  ['rooms_95',['rooms',['../namespacecluedo__fsm.html#af8cdf1a1ec60c201d772eac5c30c0e84',1,'cluedo_fsm.rooms()'],['../namespace_default_settings.html#ad3b134bd34801edab1221040684515d6',1,'DefaultSettings.rooms()']]]
+];
---
+++ cluedo/html/search/variables_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['settings_96',['settings',['../namespace_default_settings.html#a0fe96055cff83b8ba47d834d1689d0ed',1,'DefaultSettings']]],
+  ['srv_5fhint_5fclient_97',['srv_hint_client',['../classcluedo__fsm_1_1hint__gen.html#ac9bdd350b870419c32282f4c19ba1737',1,'cluedo_fsm::hint_gen']]],
+  ['srv_5foracle_5fclient_98',['srv_oracle_client',['../classcluedo__fsm_1_1oracle__query.html#a449e7465f76bfa23bd82b98bef7c9b68',1,'cluedo_fsm::oracle_query']]],
+  ['suspects_99',['suspects',['../namespacecluedo__fsm.html#acad4225c34fb7286e9d9186c04612003',1,'cluedo_fsm.suspects()'],['../namespace_default_settings.html#abeff0ac2ab861b2d6b4a99cb9b585020',1,'DefaultSettings.suspects()']]]
+];
